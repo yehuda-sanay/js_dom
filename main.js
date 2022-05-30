7b1b5d09a0e8a4067221bab87eac5672ff87410e
yehuda-sanay <yehudasanay1@gmail.com>
2022-05-30 23:58:01 +0300

    30.05.22

---
+++ main.js
@@ -0,0 +1,27 @@
+//שאלה 1
+
+// console.log(document.getElementById("num1"));
+
+//שאלה 2
+
+// document.getElementById("num2").innerText="my dom app";
+
+ //שאלה 3
+
+ 
+// var number3= document.getElementsByClassName("num3");
+// for(var i=0;i<4;i++){
+// console.log(number3[i]);
+// }
+// //שאלה 4
+// console.log(number3[2].innerHTML);
+// //שאלה 5
+// console.log(number3[3].innertext);
+
+var number6= document.getElementsByClassName("num6");
+for(var i=0;i<4;i++){
+    console.log(number6[i].innerHTML);
+}
+
+
+
